dbe2187f44b18b3d8c7366985ebca08bc4d74882
panama69 <github@flynnshome.com>
2018-09-08 12:03:11 -0500

    changed package for Micro Focus, enhanced the pom clean goal, updated READEME

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # LeanFT & Selenium
 This is a simple script to show using a Selenium test being augmented with LeanFT.
 
-This script has been run using the Chrome driver 2.36.  You can choose others to but just be aware i havent verified it against other.
+This script has been run using the Chrome driver 2.41.  You can choose others too but just be aware i havent verified it against others.
 
 The ![getDrivers.sh](getDrivers.sh) is a small script to help in pulling down different versions of the Chrome drivers.  Make sure you are in the root directory of your choosing (typically, /opt/selenium) and execute like:
 ```
 ./getDrivers.sh 2.36
 ```
 
-and the driver will be downloaded and extracted to the subfolder 2.36.
+and the driver will be downloaded and extracted to the subfolder 2.41.
 
 If you are making use of a different driver and or location, make sure to reflect the change in the actual script where the Chrome driver is deployed.
--- src/main/java/net/mf/SeleniumTest.java
+++ src/main/java/net/mf/SeleniumTest.java
@@ -1,13 +1,18 @@
 package net.mf;
+import static org.junit.Assert.*;
+import static org.openqa.selenium.support.ui.ExpectedConditions.*;
 
 import java.awt.image.RenderedImage;
 import java.io.File;
 import java.net.URI;
-import java.util.regex.Pattern;
-
 
 import org.junit.*;
+import org.openqa.selenium.support.pagefactory.ByChained;
 import org.openqa.selenium.*;
+import com.hpe.leanft.selenium.By;
+import com.hpe.leanft.selenium.ByEach;
+import java.util.regex.Pattern;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.*;
 import org.openqa.selenium.support.ui.*;
@@ -17,15 +22,14 @@ import com.hp.lft.sdk.web.*;
 import com.hp.lft.report.*;
 import com.hp.lft.verifications.*;
 import com.hpe.leanft.selenium.Utils;
-import com.hpe.leanft.selenium.ByEach;
-import com.hpe.leanft.selenium.By;
 
-public class SeleniumTest  {
+
+public class SeleniumTest {
     //private static String TEST_URL = "http://www.advantageonlineshopping.com";
     private static String TEST_URL = "http://nimbusserver:8000/";
 
     public SeleniumTest() {
-    //Change this constructor to private if you supply your own public constructor
+        //Change this constructor to private if you supply your own public constructor
     }
 
     @BeforeClass
@@ -41,7 +45,7 @@ public class SeleniumTest  {
         // initialize the SDK and report only once per process
         // this needs to be done since you are using a different framework than LFT.
         // More info at https://admhelp.microfocus.com/leanft/en/14.03/HelpCenter/Content/HowTo/CustomFrameworks.htm
-        try{
+        try {
 
             ModifiableSDKConfiguration config = new ModifiableSDKConfiguration();
             config.setServerAddress(new URI("ws://localhost:5095"));
@@ -50,8 +54,7 @@ public class SeleniumTest  {
             Reporter.init();
             Reporter.setReportLevel(ReportLevel.All);
             Reporter.setSnapshotCaptureLevel(CaptureLevel.All);
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             System.out.println("ERROR OCCURRED: ");
             e.printStackTrace();
         }
@@ -70,53 +73,53 @@ public class SeleniumTest  {
 
         // Location of where your chromedriver is locate.
         // If you don't use the setPropery, then you will need to have chromedriver in your system path
-        System.setProperty("webdriver.chrome.driver", "./2.36/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "./2.41/chromedriver");
         ChromeOptions co = new ChromeOptions();
         co.addExtensions(new File("/opt/leanft/Installations/Chrome/Agent.crx")); // path to agent on my linux yours may differ
-        co.addArguments("disable-infobars");
+        co.addArguments("disable-infobars"); // disables the annoying notification from Chrome that an automated tool is driving the browser
         WebDriver driver = new ChromeDriver(co);
         //driver.manage().window().setSize(new Dimension(945, 850));
 
-        WebDriverWait w = new WebDriverWait(driver,20);
+        WebDriverWait w = new WebDriverWait(driver, 20);
         w.ignoring(NoSuchElementException.class);
         org.openqa.selenium.WebElement P = null;
 
         try {
-            Reporter.reportEvent("Launch Selenium webdriver", "<b>Using URL:</b><br><h1><a href='"+TEST_URL+"' target='_blank'>"+TEST_URL+"</h1>");
+            Reporter.reportEvent("Launch Selenium webdriver", "<b>Using URL:</b><br><h1><a href='" + TEST_URL + "' target='_blank'>" + TEST_URL + "</h1>");
 
             driver.get(TEST_URL);
 
             // This line is using the Micro Focus  extension of Selenium through the new attribute 'visibleText'
             // I often find the WebDriverWait to be not very reliable and often I need to put in Thread.sleep ()
-            w.until(ExpectedConditions.visibilityOfElementLocated(By.visibleText("TABLETS")));  //this is one way to sync on objects in Selenium
+            w.until(visibilityOfElementLocated(By.visibleText("TABLETS")));  //this is one way to sync on objects in Selenium
 
             driver.findElement(By.visibleText("TABLETS")).click();
 
 
             // This line is using normal Selenium attributes
             Thread.sleep(2000);
-            w.until(ExpectedConditions.visibilityOfElementLocated(By.id("accordionPrice")));  //this is one way to sync on objects in Selenium
+            w.until(visibilityOfElementLocated(By.id("accordionPrice")));  //this is one way to sync on objects in Selenium
             we = driver.findElement(By.id("accordionPrice"));
             we.click();
 
             Utils.highlight(we, 1000);
             RenderedImage snapshot = Utils.getSnapshot(we);
-            Reporter.reportEvent("Accordion Price", "", Status.Passed,snapshot);
+            Reporter.reportEvent("Accordion Price", "", Status.Passed, snapshot);
 
             we = driver.findElement(new ByEach(
-                        By.tagName("a"),
-                        By.visibleText(Pattern.compile("\\$1,00\\d\\.\\d\\d"))
-                        ));
+                    By.tagName("a"),
+                    By.visibleText(Pattern.compile("\\$1,00\\d\\.\\d\\d"))
+            ));
 
             Utils.highlight(we, 1000);
 
             Verify.areEqual("$1,009.99", we.getText());
-            Verify.areEqual ("$1,009.00", we.getText(), "Price Verification","Verify price displayed matches price expected", Utils.getSnapshot(we));
+            Verify.areEqual("$1,009.00", we.getText(), "Price Verification", "Verify price displayed matches price expected", Utils.getSnapshot(we));
 
 
             // ---------- Switch to having UFT Pro (LeanFT) work on the browser ----------
             // The following will attach LeanFT to the browser opened by Selenium and work using LeanFT libraries
-            Reporter.reportEvent("Attach to Browser","Current URL: "+driver.getCurrentUrl());
+            Reporter.reportEvent("Attach to Browser", "Current URL: " + driver.getCurrentUrl());
             Browser browser = BrowserFactory.attach(new BrowserDescription.Builder().url(driver.getCurrentUrl()).build());
 
             ImageDescription imageDescription = new ImageDescription.Builder()
@@ -129,11 +132,9 @@ public class SeleniumTest  {
             browser.describe(Image.class, imageDescription).click();
 
 
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             Reporter.reportEvent("Test Fail", "Error in script execution<br>Check the error thrown", Status.Failed, e);
-        }
-        finally {
+        } finally {
             driver.quit();
         }
 
